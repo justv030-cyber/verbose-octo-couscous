88e665f30c660f7a61f9d46c7be42e8829b6ce07
justv030-cyber <justv030@gmail.com>
2026-09-09 01:05:00 +0530

    add withdraw contract balance and mint function updaate

---
+++ main.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.7.0
+pragma solidity ^0.8.27;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
+import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+contract Main is
+    ERC1155,
+    Ownable,
+    ERC1155Pausable,
+    ERC1155Burnable,
+    ERC1155Supply
+{
+    uint256 constant PUBLIC_PRICE = 0.5 ether;
+
+    uint256 constant MAX_SUPPLY = 1000;
+
+    constructor(
+        address initialOwner
+    )
+        ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
+        Ownable(initialOwner)
+    {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(uint256 id, uint256 amount) public payable {
+        require(msg.value == PUBLIC_PRICE * amount, "Not Enough Money");
+        require(totalSupply(id) + amount <= MAX_SUPPLY, "Max Supply Reached");
+        _mint(msg.sender, id, amount, "");
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values
+    ) internal override(ERC1155, ERC1155Pausable, ERC1155Supply) {
+        super._update(from, to, ids, values);
+    }
+
+    function withdrawCbalance(address _address) private onlyOwner  {
+        uint256 contractBalance = address(this).balance;
+        (bool sucess, ) = payable(_address).call{value: contractBalance}("");
+
+        require(sucess, "Transfer Failed");
+    }
+}
